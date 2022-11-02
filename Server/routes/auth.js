3c81365bea6a640014090f802b56d4250dd85a48
Camila Bezerril <camilabezerril@outlook.com>
2022-11-02 13:58:52 -0300

    fix(signup): cadastro unico para tipos de usuario

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -6,67 +6,76 @@ const utilsAuth = require("../utils/auth");
 
 router.post("/signup-geral", async (req, res) => {
   try {
-      const novoUsuario = new UsuarioGeral({
+    const usuarioNgoExistente = await UsuarioNgo.findOne({
       usuario: req.body.username,
-      nome: req.body.displayName,
-      senha: await utilsAuth.gerarSenhaComHash(req.body.password),
-      pais: req.body.originCountry,
-      idiomas: req.body.languages 
     });
 
-    const usuario = await novoUsuario.save();
-    res.status(200).json(usuario);
-    
-  } catch (err) {
-    if(err.code == 11000)
+    if (!usuarioNgoExistente) {
+      const novoUsuario = new UsuarioGeral({
+        usuario: req.body.username,
+        nome: req.body.displayName,
+        senha: await utilsAuth.gerarSenhaComHash(req.body.password),
+        pais: req.body.originCountry,
+        idiomas: req.body.languages,
+      });
+
+      const usuario = await novoUsuario.save();
+      res.status(200).json(usuario);
+    } else {
       res.status(500).json("Username is already being used.");
-    else
-      res.status(500).json("Server error.");
+    }
+  } catch (err) {
+    res.status(500).json("Server error.");
   }
 });
 
 router.post("/signup-ngo", async (req, res) => {
-    try {
-        const novoUsuario = new UsuarioNgo({
-          usuario: req.body.username,
-          nome: req.body.displayName,
-          senha: await utilsAuth.gerarSenhaComHash(req.body.password),
-          pais: req.body.originCountry,
-          idiomas: req.body.languages,
-          email: req.body.email,
-          endereco:req.body.adress
+  try {
+    const usuarioGeralExistente = await UsuarioGeral.findOne({
+      usuario: req.body.username,
+    });
+
+    if (!usuarioGeralExistente) {
+      const novoUsuario = new UsuarioNgo({
+        usuario: req.body.username,
+        nome: req.body.displayName,
+        senha: await utilsAuth.gerarSenhaComHash(req.body.password),
+        pais: req.body.originCountry,
+        idiomas: req.body.languages,
+        email: req.body.email,
+        endereco: req.body.adress,
       });
-  
+
       const usuario = await novoUsuario.save();
       res.status(200).json(usuario);
-      
-    } catch (err) {
-      if(err.code == 11000)
-      res.status(500).json("Username is already being used.");
-      else
-        res.status(500).json("Server error.");
+    } else {
+      res.status(500).json("Username is already being used.")
     }
-  });
+  } catch (err) {
+    if (err.code == 11000)
+      res.status(500).json("Username is already being used.");
+    else res.status(500).json("Server error.");
+  }
+});
 
 router.post("/signin", async (req, res) => {
   try {
-      var usuario = await UsuarioGeral.findOne({ usuario: req.body.username });
+    var usuario = await UsuarioGeral.findOne({ usuario: req.body.username });
 
-      if(!usuario)
-        usuario = await UsuarioNgo.findOne({ usuario: req.body.username });
-      
-      !usuario && res.status(404).json("User not found.");
+    if (!usuario)
+      usuario = await UsuarioNgo.findOne({ usuario: req.body.username });
 
-      await utilsAuth.validarSenha(req.body.password, usuario.senha)
-        && res.status(400).json("Invalid password.");
+    !usuario && res.status(404).json("User not found.");
 
-      res.status(200).json(usuario);
+    (await utilsAuth.validarSenha(req.body.password, usuario.senha)) &&
+      res.status(400).json("Invalid password.");
 
-    } catch (err) {
-      //res.status(500).json("Erro no servidor.");
-      res.status = 500
-      res.status.json = "Server error."
-    }
-  });
+    res.status(200).json(usuario);
+  } catch (err) {
+    //res.status(500).json("Erro no servidor.");
+    res.status = 500;
+    res.status.json = "Server error.";
+  }
+});
 
 module.exports = router;
