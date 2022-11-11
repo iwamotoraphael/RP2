72c51bf89fc1d99c4803afc78d9629be993212e4
iwamotoraphael <iwamotoraphael@gmail.com>
2022-11-11 14:20:01 -0300

    UserPage get user data

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,20 +1,28 @@
-import React, {useState} from "react";
-import Button from "../components/Button";
+import React, {useState, useEffect} from "react";
 import Header from "../components/Header";
 import SideProfile from "../components/SideProfile";
 import Post from "../components/Post";
 
 import "../css/pages/ProfilePage.css";
 import '../css/pages/HomePage.css';
-
+import { useParams } from "react-router-dom";
+import { getUser } from "../services/api";
 
 const UserPage = () =>{
+
+    const params = useParams()
+
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        getUser(params.id).then((u) =>{setUser(u.data); console.log(u)})
+    }, [])
 
     return (
         <>
             <Header></Header>
             <div className="homepage-container profile-container">
-                <SideProfile profile_name='Teste' profile_pic='https://i.scdn.co/image/ab6775700000ee8572d6997d9fef4932a4606253' bio='teste'></SideProfile>
+                <SideProfile profile_name={user.nome} profile_pic={user.fotoPerfil} bio={user.bioDesc}></SideProfile>
                 <div className="post-wrapper">
 
                     <Post post_name='Teste' post_date='2022-09-21' post_content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro vitae, atque nulla repudiandae quae corporis, tenetur officia, natus deserunt error accusamus ex dolorem rem quod nesciunt ducimus qui dicta laborum!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro vitae, atque nulla repudiandae quae corporis, tenetur officia, natus deserunt error accusamus ex dolorem rem quod nesciunt ducimus qui dicta laborum!Lorem ipsum dolor sit, aaaaaaaaaa' post_id={0}></Post>
